03427266c313b21bad449a4c9dcd747998694d13
Emmanuel Tetteh-Agor <emmanueltettehagor74@gmail.com>
2024-11-29 10:26:11 +0000

    Added sm:w-24 and sm:h-24 to the img class

--- tailwind-react-integration/src/components/UserProfile.js
+++ tailwind-react-integration/src/components/UserProfile.js
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="rounded-full sm:w-24 sm:h-24 w-28 h-28 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
