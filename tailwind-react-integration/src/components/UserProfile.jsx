281267f515f93bcf7dcce7b8771d734c9dbed6bf
Emmanuel Tetteh-Agor <emmanueltettehagor74@gmail.com>
2024-11-29 10:22:22 +0000

    added md:text-xl

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
+      <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 text-center hover:text-blue-500 transition-colors duration-300 ease-in-out">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center">
